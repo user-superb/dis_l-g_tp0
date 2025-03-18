56abfda0a99c8624db28087f23707e34cbddd692
superb <maiden.heaven001@gmail.com>
2025-03-18 13:40:19 -0300

    Added .gitignore and updated main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
+#include <stdio.h>
+
 #include "util.h"
 
-int main() {
-	printBinary(12345);
+int main() 
+{
+	char opt = '0';
+
+	printf("Seleccionar n° Opcion: \n1: f) \n2: g) \n3: h) \n:> ");
+	scanf("%c", &opt);
+
+	switch (opt) 
+	{
+		case '1':
+			printf(":)))))))))\n");
+		break;
+
+		default:
+		break;
+	}
+
 	return 0;
 }
